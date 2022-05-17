e886daf38fab9d8720885ca3f3bb4bdbfe4a432d
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2022-05-17 19:01:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
             <li><a href="#">Home</a></li>
             <li>Basic</li>
           </ol>
-          <h1>Welcome IOCL team to this session on OpenShift! We thank you for taking out the time to attend this session and hope you all are doing well.</h1>
+          <h1>Welcome you all to this brief overview on OpenShift! Red Hat thanks you all for taking out the time and hopes you all are doing well.</h1>
           <p>The purpose of this application is to demonstrate several interesting features about OpenShift capabilities. We hope you enjoy it!</p>
         </div>
       </div>
